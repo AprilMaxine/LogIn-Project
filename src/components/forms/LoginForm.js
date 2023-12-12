00cd1753692f4f6530a9fff151ba4ce212ef8343
AprilMaxine <aprilmaxine.baculio2001@gmail.com>
2023-12-12 20:47:26 +0800

    Login/Registration

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,68 +1,94 @@
-import React from "react";
-import { View } from "react-native";
-import { Button, Text, TextInput, StyleSheet } from "react-native-paper";
-import { createStackNavigator } from "@react-navigation/stack";
+import React, { useState } from "react";
+import { View, Alert } from "react-native";
+import { Button, Text, TextInput } from "react-native-paper";
 import styles from "../config/styles";
 
-const Stack = createStackNavigator();
+const LoginForm = ({ navigation }) => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-export default function LoginForm({ navigation }) {
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
+    const handleLogin = async () => {
+        try {
+            const response = await fetch(`http://192.168.200.202/login.php?email=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}`);
 
-  return (
-    <View style={styles.container}>
-      <Text variant="displayMedium">Login Account</Text>
-      <Text>Enter your email address and password to log in.</Text>
-      <TextInput
-        mode="outlined"
-        placeholder="Email"
-        label="Email"
-        style={{ marginTop: 20 }}
-        value={email}
-        onChangeText={(text) => setEmail(text)}
-      />
-      <TextInput
-        mode="outlined"
-        placeholder="Password"
-        label="Password"
-        secureTextEntry
-        style={{ marginTop: 10 }}
-        value={password}
-        onChangeText={(text) => setPassword(text)}
-      />
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
 
-      <Text
-        onPress={() => navigation.navigate("Recover")}
-        style={{ marginTop: 10, color: "#3498db", textDecorationLine: 'underline', alignSelf: 'flex-end', }}
-      >
-        Forgot Password
-      </Text>
-      <Button
-        onPress={() => navigation.navigate("Home")}
-        mode="contained"
-        style={{ marginTop: 25, backgroundColor: "#3498db" }}
-        labelStyle={{ color: "white" }}
-      >
-        Login
-      </Button>
-      <Button
-        onPress={() => navigation.navigate("Register")}
-        mode="contained"
-        style={{ marginTop: 10, backgroundColor: "#3498db" }}
-        labelStyle={{ color: "white" }}
-      >
-        Register Account
-      </Button>
-      <Button
-        onPress={() => navigation.pop()}
-        icon="arrow-left"
-        mode="contained"
-        style={{ marginTop: 10, backgroundColor: "#3498db" }}
-        labelStyle={{ color: "white" }}
-      >
-        Back
-      </Button>
-    </View>
-  );
-}
+            const data = await response.json();
+
+            if (data.success) {
+                // Login successful, you can navigate to the home screen or perform other actions
+                navigation.navigate("Home");
+            } else {
+                // Login failed, show an alert or handle the error accordingly
+                Alert.alert("Login Failed", data.message);
+            }
+        } catch (error) {
+            console.error("Error during login:", error.message);
+            Alert.alert("An error occurred during login");
+        }
+    };
+
+    return (
+        <View style={styles.container}>
+            <Text variant="displayMedium">Login Account</Text>
+            <Text>Enter your email address and password to log in.</Text>
+            <TextInput
+                mode="outlined"
+                placeholder="Email"
+                label="Email"
+                style={{ marginTop: 20 }}
+                value={email}
+                onChangeText={(text) => setEmail(text)}
+            />
+            <TextInput
+                mode="outlined"
+                placeholder="Password"
+                label="Password"
+                secureTextEntry
+                style={{ marginTop: 10 }}
+                value={password}
+                onChangeText={(text) => setPassword(text)}
+            />
+            <Text
+                onPress={() => navigation.navigate("Recover")}
+                style={{
+                    marginTop: 10,
+                    color: "#3498db",
+                    textDecorationLine: "underline",
+                    alignSelf: "flex-end",
+                }}
+            >
+                Forgot Password
+            </Text>
+            <Button
+                onPress={handleLogin}
+                mode="contained"
+                style={{ marginTop: 25, backgroundColor: "#3498db" }}
+                labelStyle={{ color: "white" }}
+            >
+                Login
+            </Button>
+            <Button
+                onPress={() => navigation.navigate("Register")}
+                mode="contained"
+                style={{ marginTop: 10, backgroundColor: "#3498db" }}
+                labelStyle={{ color: "white" }}
+            >
+                Register Account
+            </Button>
+            <Button
+                onPress={() => navigation.pop()}
+                icon="arrow-left"
+                mode="contained"
+                style={{ marginTop: 10, backgroundColor: "#3498db" }}
+                labelStyle={{ color: "white" }}
+            >
+                Back
+            </Button>
+        </View>
+    );
+};
+
+export default LoginForm;
--- src/components/forms/RegisterForm.js
+++ src/components/forms/RegisterForm.js
@@ -1,22 +1,78 @@
-import { View } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import { View, Alert, ActivityIndicator } from "react-native";
 import { Button, Text, TextInput } from "react-native-paper";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import styles from "../config/styles";
-const Stack = createNativeStackNavigator();
 
-export default function RegisterForm({ navigation }) {
-  const [showPass, setShowPass] = React.useState(false);
-  const [showRePass, setShowRePass] = React.useState(false);
+const RegisterForm = ({ navigation }) => {
+  const [showPass, setShowPass] = useState(false);
+  const [showRePass, setShowRePass] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [rePassword, setRePassword] = useState("");
+
+  const handleRegister = async () => {
+    if (!name || !email || !password || !rePassword) {
+      Alert.alert("All fields are required");
+      return;
+    }
+
+    if (password !== rePassword) {
+      Alert.alert("Passwords do not match");
+      return;
+    }
+
+    setLoading(true);
+
+    try {
+      const response = await fetch("http:192.168.200.202/registration.php", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: `name=${encodeURIComponent(name)}&email=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}`,
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+
+      if (data.success) {
+        Alert.alert("Registration successful!");
+        navigation.navigate("Login");
+      } else {
+        Alert.alert("Registration failed", data.message || "An error occurred");
+      }
+    } catch (error) {
+      console.error("Error during registration:", error.message);
+      Alert.alert("An error occurred during registration");
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text variant="displayMedium">Register</Text>
       <TextInput
         mode="outlined"
+        placeholder="Name"
+        label="Name"
+        style={{ marginTop: 10 }}
+        value={name}
+        onChangeText={(text) => setName(text)}
+      />
+      <TextInput
+        mode="outlined"
         placeholder="Email"
         label="Email"
         style={{ marginTop: 10 }}
-        error={true}
+        value={email}
+        onChangeText={(text) => setEmail(text)}
       />
       <TextInput
         mode="outlined"
@@ -30,6 +86,8 @@ export default function RegisterForm({ navigation }) {
           />
         }
         style={{ marginTop: 10 }}
+        value={password}
+        onChangeText={(text) => setPassword(text)}
       />
       <TextInput
         mode="outlined"
@@ -38,32 +96,37 @@ export default function RegisterForm({ navigation }) {
         secureTextEntry={showRePass}
         right={
           <TextInput.Icon
-            icon={!showPass ? "eye" : "eye-off"}
+            icon={!showRePass ? "eye" : "eye-off"}
             onPress={() => setShowRePass(!showRePass)}
           />
         }
         style={{ marginTop: 10 }}
+        value={rePassword}
+        onChangeText={(text) => setRePassword(text)}
       />
-  
+
       <Button
-        onPress={() => navigation.navigate("Login")}
+        onPress={handleRegister}
         mode="contained"
         style={{ marginTop: 10, backgroundColor: "#3498db" }}
         labelStyle={{ color: "white" }}
-        
-        >
-      Register
+        disabled={loading}
+      >
+        {loading ? <ActivityIndicator color="white" /> : "Register"}
       </Button>
-    
+
       <Button
         onPress={() => navigation.pop()}
         icon="arrow-left"
         mode="contained"
-        style={{ marginTop: 10, backgroundColor: "#3498db" }} 
+        style={{ marginTop: 10, backgroundColor: "#3498db" }}
         labelStyle={{ color: "white" }}
+        disabled={loading}
       >
         Back
       </Button>
     </View>
   );
-}
+};
+
+export default RegisterForm;
